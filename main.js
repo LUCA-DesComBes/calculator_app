25ae5f4ae23177377f756bd243dcbbe47183f51c
BiscuitCuit15 <ldescombes@edenschool.fr>
2025-01-08 09:22:08 +0100

    feat (app) : calculator add

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
 const deleteBtn = document.querySelector("#delete");
+const equalBtn = document.querySelector("#equal")
+const resetBtn = document.querySelector("#reset")
 const simpleBtn = document.querySelectorAll(".simple")
 let number = document.querySelector("#num");
 
 simpleBtn.forEach((btn) => {
     btn.addEventListener("click", ()=>{
+        // if(number.innerText == "0") {
+        //     number.i
+        // }
         number.innerText += btn.innerText;
     })
 })
 deleteBtn.addEventListener("click", () => {
     number.innerText = number.innerText.slice(0, -1)
+})
+resetBtn.addEventListener("click", () => {
+    number.innerText = "";
+})
+equalBtn.addEventListener("click", () => {
+    number.innerText = eval(number.innerText)
 })
